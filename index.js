51c7252bca23e85f327a10371c8687f152440a53
Dylan Thinnes <dylan.j.thinnes@gmail.com>
2018-01-21 21:48:04 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,175 @@
+/* authenticator defines authentication middleware and helper functions, namely:
+authenticatePassword, which takes a request and checks its password for validity with in-database hashes. On success, it responds to the user with an allocated session token.
+authenticateSession, which is middleware which calls next if isSession evaluates to true, and 403/401s otherwise.
+isSession returns a Promise which resolves if the username and sessionToken are validly associated with one another (passing in true/false depending on use of memory) and rejects otherwise.
+*/
+var crypto = require("crypto");
+var abstractdb = require("abstractdb");
+var Auth = function (databaseType) {
+    if (databaseType === "sqlite") this.Database = abstractdb("better-sqlite3");
+    else if (databaseType == undefined) this.Database = abstractdb("better-sqlite3");
+    else throw "Unrecognized database type passed.";
+}
+module.exports = Auth;
+
+Auth.prototype.init = function (path, options) {
+    //Define databases
+    this.db = new this.Database(path, options);
+    this.db.sRun("CREATE TABLE sessions (username STRING, token STRING, timestamp INTEGER)");
+    this.db.sRun("CREATE TABLE auth (username STRING PRIMARY KEY, salt STRING, key STRING, inviteAllowed INTEGER)");
+    this.db.sRun("CREATE TABLE invites (issuer STRING, code STRING)");
+}
+Auth.prototype.getSaltAndKey = async function (username) {
+    return await this.db.aGet("SELECT salt, key FROM auth WHERE username=$username", {
+        $username: username
+    });
+}
+Auth.prototype.checkPassword = async function (username, password) {
+    var saltAndKey = await this.getSaltAndKey(username);
+    if (saltAndKey == undefined || saltAndKey.salt == undefined || saltAndKey.key == undefined) return false;
+    var createdKey = crypto.pbkdf2Sync(password, saltAndKey.salt, 100000, 64, "sha512").toString("hex");
+    if (saltAndKey.key === createdKey) return true;
+    return false;
+}
+Auth.prototype.newSessionToken = async function (username) {
+    var sessionToken = crypto.randomBytes(256).toString("hex");
+    var result = await this.db.aRun("INSERT INTO sessions VALUES ($username, $sessionToken, $timestamp)", {
+        $username: username,
+        $sessionToken: sessionToken,
+        $timestamp: Date.now()
+    });
+    return sessionToken;
+}
+Auth.prototype.getSessionToken = async function (username) {
+    var result = await this.db.aGet("SELECT token FROM sessions WHERE username=$username", {
+        $username: username
+    });
+    if (result != undefined && result.token != undefined) return result.token;
+    return await this.newSessionToken(username);
+}
+Auth.prototype.createUser = async function (username, password) { //createUser first checks if a user with that username exists, if not it creates that user.
+    var result = await this.db.aGet("SELECT username FROM auth WHERE username=$username", {
+        $username: username
+    });
+    if (result != undefined) return false;
+    var salt = crypto.randomBytes(256).toString("hex");
+    await this.db.aRun("INSERT INTO auth VALUES ($username, $salt, $key, $inviteAllowed)", {
+        $username: username,
+        $salt: salt,
+        $key: crypto.pbkdf2Sync(password, salt, 100000, 64, "sha512").toString("hex"),
+        $inviteAllowed: 0
+    });
+    return true;
+}
+Auth.prototype.useInvite = async function (code) {
+    var result = await this.db.aGet("SELECT issuer FROM invites WHERE code=$code", {
+        $code: code
+    });
+    if (result == undefined) return false;
+    await this.db.aRun("DELETE FROM invites WHERE code=$code", {
+        $code: code
+    });
+    return true;
+}
+Auth.prototype.issueInvite = async function (username, password) {
+    var result = await Promise.all([
+        this.db.aGet("SELECT username FROM auth WHERE username=$username AND inviteAllowed=1", {
+            $username: username
+        }),
+        this.checkPassword(username, password)
+    ]);
+    var inviteIsAllowed = result[0] != undefined;
+    var validPassword = result[1];
+    if (inviteIsAllowed === false || validPassword === false) return undefined;
+    var code = crypto.randomBytes(8).toString("hex");
+    await this.db.aRun("INSERT INTO invites VALUES ($issuer, $code)", {
+        $issuer: username,
+        $code: code
+    });
+    return code;
+}
+Auth.prototype.signup = async function (req, res) {
+    var username = req.body.username;
+    var password = req.body.password;
+    var inviteCode = req.body.inviteCode;
+    var overrideInviteCode = req.overrideInviteCode === true ? true : false; //Force overrideInviteCode to be a Boolean to prevent potential type conversion shenanigans
+    if (username == undefined || password == undefined || (inviteCode == undefined && !overrideInviteCode)) {
+        res.status(400);
+        res.end("You have not provided the necessary data to register an account.");
+        return false;
+    }
+    var usernameTooLong = username.length > 30;
+    var passwordTooLong = password.length > 100;
+    var specialCharsSearch = /([\x00-\x1F]|\s)/;
+    var usernameHasSpecialChars = specialCharsSearch.test(username);
+    var passwordHasSpecialChars = specialCharsSearch.test(password);
+    if (usernameTooLong || passwordTooLong || usernameHasSpecialChars || passwordHasSpecialChars) {
+        res.status(400);
+        res.end("The password xor/and username provided is/are not valid. Either too long, or containing control characters/whitespace.");
+        return false;
+    }
+    var userAlreadyExists = await this.getSaltAndKey(username) != undefined; //If getSaltAndKey returns undefined then there must be no user with that username
+    if (userAlreadyExists) {
+        res.status(400);
+        res.end("Account with username " + username + " already exists. Account not created.");
+        return false;
+    }
+    var inviteIsValid = await this.useInvite(inviteCode);
+    if (!inviteIsValid && !overrideInviteCode) {
+        res.status(400);
+        res.end("Invite code is not valid.");
+        return false;
+    }
+    await this.createUser(username, password);
+    res.status(200);
+    res.end("Account with username " + username + " and password " + ("").padStart(password.length, "*") + " registered.");
+    return true;
+}
+Auth.prototype.authenticatePassword = async function (req, res) {
+    var username = req.body.username;
+    var password = req.body.password;
+    if (username !== undefined && password !== undefined) {
+        var validLogin = await this.checkPassword(username, password);
+        if (validLogin) {
+            res.status(200);
+            var token = await this.getSessionToken(username);
+            res.cookie("username", username);
+            res.cookie("sessionToken", token);
+            res.end(token);
+            return true;
+        } else {
+            res.status(403);
+            res.end("Your credentials do not have authorization for this zone.");
+            return false;
+        }
+    } else {
+        res.status(401);
+        res.end("You have not supplied any credentials for this zone.");
+        return false;
+    }
+}
+Auth.prototype.authenticateSession = async function (req, res, next) {
+    var sessionToken = req.cookies.sessionToken;
+    var username = req.cookies.username;
+    if (username == undefined || sessionToken == undefined) {
+        res.status(401);
+        res.end("You have not supplied any credentials for this zone.");
+        return false;
+    }
+    var sessionIsValid = await this.isSession(username, sessionToken);
+    if (!sessionIsValid) {
+        res.status(403);
+        res.end("Your credentials do not have the authorization for this zone.");
+        return false;
+    }
+    next();
+    return true;
+}
+Auth.prototype.isSession = async function (username, token) {
+    var result = this.db.aGet("SELECT username FROM sessions WHERE token=$token AND username=$username", {
+        $username: username,
+        $token: token
+    });
+    if (result) return true;
+    else return false;
+}
---
+++ test.js
@@ -0,0 +1,209 @@
+(async function () {
+    var Auth = require("./index.js");
+    var auth = new Auth("sqlite");
+    var sampleUsername1 = "mark";
+    var samplePassword1 = "1234";
+    var sampleUsername2 = "jake";
+    var samplePassword2 = "abcd";
+    var SampleResponse = function (endHandler) { //SampleResponse creates mock expressjs response objects with status and end methods
+        this.code = 0;
+        this.status = (function (code) {
+            this.code = code;
+        }).bind(this);
+        this.end = endHandler.bind(this);
+        this.setHeader = function () {}
+        this.cookie = function () {}
+    }
+
+    //Database functions should return errors on database errors, such as when their tables aren't yet CREATEd
+    try {
+        var res = await auth.createUser(sampleUsername1, samplePassword1);
+        console.log("Failed: createUser doesn't reject for database error.");
+    } catch (e) {
+        console.log("Passed: createUser rejects for database error.");
+    }
+    try {
+        var res = await auth.allocateAuthToken(sampleUsername1);
+        console.log("Failed: allocateAuthToken doesn't reject for database error.");
+    } catch (e) {
+        console.log("Passed: allocateAuthToken rejects for database error.");
+    }
+    try {
+        var res = await auth.checkPassword(sampleUsername1, samplePassword1);
+        console.log("Failed: checkPassword doesn't reject for database error.");
+    } catch (e) {
+        console.log("Passed: checkPassword rejects for database error.");
+    }
+    try {
+        var res = await auth.getSaltAndKey(sampleUsername1, samplePassword1);
+        console.log("Failed: getSaltAndKey doesn't reject for database error.");
+    } catch (e) {
+        console.log("Passed: getSaltAndKey rejects for database error.");
+    }
+    try {
+        var res = await auth.issueInvite(sampleUsername1, samplePassword1);
+        console.log("Failed: issueInvite doesn't reject for database error.");
+    } catch (e) {
+        console.log("Passed: issueInvite rejects for database error.");
+    }
+    try {
+        var res = await auth.useInvite(sampleUsername1, samplePassword1);
+        console.log("Failed: useInvite doesn't reject for database error.");
+    } catch (e) {
+        console.log("Passed: useInvite rejects for database error.");
+    }
+
+    //CREATE tables for next tests
+    /*
+    await auth.db.aRun("CREATE TABLE auth (username STRING, salt STRING, key STRING, inviteAllowed INTEGER)");
+    await auth.db.aRun("CREATE TABLE invites (issuer STRING, code STRING)");
+    await auth.db.aRun("CREATE TABLE sessions (username STRING, token STRING, timestamp INTEGER)");
+    */
+    auth.init("memory1.db", { memory: true });
+
+    //Make a sample request for password authentication. Fails because auth database is currently empty.
+    req = { body: { password: samplePassword1, username: sampleUsername1 } }
+    res = new SampleResponse(function (data) {
+        if (this.code === 0) throw ".end called before .status, check auth.js for errors.";
+        else if (this.code === 200) console.log("Failed: authenticatePassword request to empty database with no password validated as correct.");
+        else if (this.code === 403) console.log("Passed: authenticatePassword request to empty database rejected as no valid password-user pair exists.");
+        else if (this.code === 401) console.log("Failed: authenticatePassword request to empty database rejected on false grounds of there being no password or username in the body.");
+        else throw "Unexpected status code, not 0, 200, 403, or 401";
+    });
+    await auth.authenticatePassword(req, res);
+
+
+    //Make another sample request, this time with failing body data. Do this for three 401 states, and a sample 403.
+    req1 = { body: { username: sampleUsername1 } }
+    req2 = { body: { password: samplePassword1 } }
+    req3 = { body: {} }
+    req4 = { body: { username: sampleUsername2, password: samplePassword2 } }
+    res401 = new SampleResponse(function (data) {
+        if (this.code === 0) throw ".end called before .status, check auth.js for errors.";
+        else if (this.code === 200) console.log("Failed: authenticatePassword request returns valid status despite invalid body data.");
+        else if (this.code === 403) console.log("Failed: authenticatePassword request returns 403 where a 401 should do.");
+        else if (this.code === 401) console.log("Passed: authenticatePassword request returns 401 where body does not contain required data.");
+        else throw "Unexpected status code, not 0, 200, 403, or 401";
+    });
+    res403 = new SampleResponse(function (data) {
+        if (this.code === 0) throw ".end called before .status, check auth.js for errors.";
+        else if (this.code === 200) console.log("Failed: authenticatePassword request returns 200 where body has nonexistent credentials.");
+        else if (this.code === 403) console.log("Passed: authenticatePassword request returns 403 where body has nonexistent credentials.");
+        else if (this.code === 401) console.log("Failed: authenticatePassword request returns 401 where credentials were supplied.");
+        else throw "Unexpected status code, not 0, 200, 403, or 401";
+    });
+    await auth.authenticatePassword(req1, res401);
+    res401.status(0);
+    await auth.authenticatePassword(req2, res401);
+    res401.status(0);
+    await auth.authenticatePassword(req3, res401);
+    await auth.authenticatePassword(req4, res403);
+
+    //Call signup without sufficient data.
+    signupReq1 = { body: { password: samplePassword1, username: sampleUsername1, inviteCode: undefined } }
+    signupReq2 = { body: { password: samplePassword1, username: undefined, inviteCode: "abc123" } }
+    signupReq3 = { body: { password: undefined, username: sampleUsername1, inviteCode: undefined } }
+    signupRes = new SampleResponse(function (message) {
+        if (this.code === 0) throw "Error: .end called before .status, check auth.js for errors.";
+        if (this.code === 200) console.log("Failed: User signed up where body does not contain required data.");
+        else if (this.code === 400) console.log("Passed: User signup rejected with 400 since body does not contain required data.");
+        else throw "Unexpected status code: " + this.code;
+    });
+    await auth.signup(signupReq1, signupRes);
+    signupRes.status(0);
+    await auth.signup(signupReq2, signupRes);
+    signupRes.status(0);
+    await auth.signup(signupReq3, signupRes);
+
+    //Call signup with invalid credentials. Either a password which is >100 chars, a username which is >30 chars, a password which contains special characters, or am inviteCode that isn't in the invite database
+    signupReq1 = { body: { password: samplePassword1, username: sampleUsername1, inviteCode: "notvalid" } } //The inviteCode is guaranteed to be invalid because all inviteCodes are in hexadecimal
+    signupReq2 = { body: { password: "abc\x0Aabc", username: sampleUsername1, inviteCode: "abc123" } } //The password has a special control character (LF) in it.
+    signupReq3 = { body: { password: "a" + ("12345").repeat(20), username: sampleUsername1, inviteCode: "abc123" } } //The password is too long (101 chars).
+    signupReq4 = { body: { password: samplePassword1, username: "a" + ("12345").repeat(6), inviteCode: "abc123" } } //The username is too long (31 chars).
+    signupRes = new SampleResponse(function (message) {
+        if (this.code === 0) throw "Error: .end called before .status, check auth.js for errors.";
+        if (this.code === 200) console.log("Failed: User signed up despite " + this.invalidMessage);
+        else if (this.code === 400) console.log("Passed: User signup rejected with 400 due to " + this.invalidMessage);
+        else throw "Unexpected status code: " + this.code;
+    });
+    signupRes.invalidMessage = "the inviteCode being invalid.";
+    await auth.signup(signupReq1, signupRes);
+    signupRes.invalidMessage = "the password containing a special control character.";
+    await auth.signup(signupReq2, signupRes);
+    signupRes.invalidMessage = "the password being too long.";
+    await auth.signup(signupReq3, signupRes);
+    signupRes.invalidMessage = "the username being too long.";
+    await auth.signup(signupReq4, signupRes);
+
+    //Call signup to create a user-key-salt triple from the user-password pair. Override conventional inviteCode requirement. Check that authenticatePassword works afterwards.
+    signupReq = {
+        body: { password: samplePassword1, username: sampleUsername1 },
+        overrideInviteCode: true
+    }
+    signupRes = new SampleResponse(function (message) {
+        if (this.code === 0) throw "Error: .end called before .status, check auth.js for errors.";
+        if (this.code === 200) console.log("Passed: User signed up with inviteOverride set to true.");
+        else if (this.code === 400) console.log("Failed: User signup rejected with 400 despite inviteOverride being set to true.");
+        else throw "Unexpected status code: " + this.code;
+    });
+    passwordReq = {
+        body: { password: samplePassword1, username: sampleUsername1 },
+    }
+    passwordRes = new SampleResponse(function (token) {
+        if (this.code === 0) throw "Error: .end called before .status, check auth.js for errors.";
+        else if (this.code === 200) {
+            console.log("Passed: authenticatePassword request validates a correct password-user pair.");
+            sessionReq.cookies.sessionToken = token;
+        } else if (this.code === 403) console.log("Failed: authenticatePassword request rejected despite there being a correct password-user pair.");
+        else if (this.code === 401) console.log("Failed: authenticatePassword request rejected on false grounds of there being no password or username in the body.");
+        else throw "Unexpected status code: " + this.code;
+    });
+    sessionReq = { cookies: { username: sampleUsername1 } }
+    sessionRes = new SampleResponse(function (isSession) {
+        if (this.code === 0) throw "Error: .end called before .status, check auth.js for errors.";
+        else if (this.code === 200) console.log("Passed: authenticateSession request validates a correct token-user pair.");
+        else if (this.code === 403) console.log("Failed: authenticateSession request rejected despite there being a correct token-user pair.");
+        else if (this.code === 401) console.log("Failed: authenticateSession request rejected on false grounds of there being no sessionToken or username in the body.");
+        else throw "Unexpected status code: " + this.code;
+    });
+    await auth.signup(signupReq, signupRes);
+    var passwordAuthenticated = await auth.authenticatePassword(passwordReq, passwordRes); //The passwordReq.end callback will automatically assign the new token to the sessionReq.body object
+
+    var sessionAuthenticated = await auth.authenticateSession(sessionReq, sessionRes, () => {}); //Do the same for sessionAuthenticated.
+    if (sessionAuthenticated) console.log("Passed: sessionAuthenticated returns true with the createUser() token and username.");
+    else console.log("Failed: sessionAuthenticated returns false with the createUser() token and username.");
+
+    var isSession = await auth.isSession(sampleUsername1, sessionReq.cookies.sessionToken); //Now that a sampleToken is established, run isSession with valid data.
+    if (isSession) console.log("Passed: isSession returns true with the createUser() token and username.");
+    else console.log("Failed: isSession returns false with the createUser() token and username.");
+
+    //Issue an invite using auth.issueInvite and the previous user's credentials. Should fail because previous user lacks the permissions.
+    var inviteCode = await auth.issueInvite(sampleUsername1, samplePassword1);
+    if (inviteCode !== undefined) console.log("Failed: inviteCode created where user should lack the permissions to do so.");
+    else console.log("Passed: inviteCode was not created because user lacked the permissions to do so.");
+
+    //Change user permissions, then issue an invite using auth.issueInvite and the previous user's credentials. Should now succeed.
+    await auth.db.aRun("UPDATE auth SET inviteAllowed=1 WHERE username=$username", { //Make the first sampleUser able to invite users.
+        $username: sampleUsername1
+    });
+    var inviteCode = await auth.issueInvite(sampleUsername1, samplePassword1);
+    if (inviteCode !== undefined) console.log("Passed: inviteCode created where user has the permissions to do so.");
+    else console.log("Failed: inviteCode was not created where user should have the permissions to do so.");
+
+    //Test the creation of a user with new credentials and an inviteCode from the previous user, without using overrideInviteCode.
+    signupReq = {
+        body: { password: samplePassword1, username: sampleUsername1, inviteCode: inviteCode },
+        overrideInviteCode: false
+    }
+    signupRes = new SampleResponse(function (token) {
+        if (this.code === 0) throw "Error: .end called before .status, check auth.js for errors.";
+        else if (this.code === 200) console.log("Passed: signup succeeded when using the newly allocated inviteCode.");
+        else if (this.code === 400) console.log("Failed: signup failed despite having valid new inviteCode.");
+        else throw "Unexpected status code: " + this.code;
+    });
+    
+
+})().catch((err) => {
+    console.log("Error in test syntax, please locate and correct.");
+    throw err;
+});
